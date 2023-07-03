a4434285390db2bc6ea8c1e22764b1edb8cabacd
Anderson André <andreandersoncaue.e@gmail.com>
2023-07-03 13:28:18 -0300

    feat: new posts in section of projects

--- data/projectsData.js
+++ data/projectsData.js
@@ -2,7 +2,7 @@ const projectsData = [
   {
     title: 'Zaffre Imóveis',
     description: `Projeto para uma imobiliária virtual sendo desenvolvida com Flutter com o objetivo de resolver um problema do mundo real`,
-    imgSrc: '/static/images/projects/zaffre/zaffre.png',
+    imgSrc: '/static/images/projects/zaffre/Zaffre-banner.png',
     href: 'https://github.com/Anderson-Andre-P/Imobis-Flutter',
   },
   {
@@ -12,16 +12,40 @@ const projectsData = [
     href: 'https://github.com/Anderson-Andre-P/coffee',
   },
   {
-    title: 'Christmas Website',
-    description: `Esse é um projeto de estudos de fim de semana que consistia em criar um website utilizando clean code e boas práticas de desenvolvimento com HTML, CSS e Js.`,
-    imgSrc: '/static/images/projects/christmas/christmas.png',
-    href: 'https://github.com/Anderson-Andre-P/christmas-website',
-  },
-  {
     title: 'Clone Twitter',
     description: `Esse é um projeto de estudos que consistia em criar um clone da rede social Twitter. Eu tinha como objetivo deixar o mais parecido possível e implementar algumas funcionalidades que a rede social também possui.`,
     imgSrc: '/static/images/projects/twitter/twitter.png',
     href: 'https://github.com/Anderson-Andre-P/clone-twitter',
+  },
+  {
+    title: 'YouTube',
+    description: `Esse é um clone do Youtube que foi feito sem a utilização de frameworks e bibliotecas JS, ou seja, foi utiizado apenas HTML, CSS e JavaScript.`,
+    imgSrc: '/static/images/projects/youtube/YoutubeClone.png',
+    href: 'https://github.com/Anderson-Andre-P/cloneYoutube',
+  },
+  {
+    title: 'Starbucks',
+    description: `Esse é um clone da landing page do Starbucks que foi feito utilizando apenas as tecnologias base do desenvolvimento web.`,
+    imgSrc: '/static/images/projects/starbucks/StarbucksClone.png',
+    href: 'https://github.com/Anderson-Andre-P/cloneStarbucks',
+  },
+  {
+    title: 'Google Ui',
+    description: `Com o objetivo de aperfeiçoar meus conhecimentos em responsividade e clen code foi desenvolvido um clone da página de buscas do Google.`,
+    imgSrc: '/static/images/projects/google/GoogleClone.png',
+    href: 'https://github.com/Anderson-Andre-P/clone-google',
+  },
+  {
+    title: 'Currículo Website',
+    description: `Esse é um antigo projeto que gostei bastante de desenvolver, ele consiste em um website no formato de currículo.`,
+    imgSrc: '/static/images/projects/resume/resume.png',
+    href: 'https://github.com/Anderson-Andre-P/curriculo',
+  },
+  {
+    title: 'Christmas Website',
+    description: `Esse é um projeto de estudos de fim de semana que consistia em criar um website utilizando clean code e boas práticas de desenvolvimento com HTML, CSS e Js.`,
+    imgSrc: '/static/images/projects/christmas/christmas.png',
+    href: 'https://github.com/Anderson-Andre-P/christmas-website',
   },
 ]
 
