feb68d034cb3425c3e615225775da77057b95761
Anderson-Andre-P <eleu.ander@gmail.com>
2023-09-16 10:22:47 -0300

    feat: att skills

--- components/Skills.js
+++ components/Skills.js
@@ -1,23 +1,18 @@
 /* eslint-disable prettier/prettier */
 import {
   SiReact,
-  SiHtml5,
-  SiCss3,
   SiJavascript,
   SiTypescript,
   SiNextdotjs,
-  SiNodedotjs,
   SiMysql,
-  SiTailwindcss,
-  SiRedux,
   SiFirebase,
   SiGit,
   SiFlutter,
   SiDart,
-  SiSass,
-  SiStyledcomponents,
-  SiAndroid,
+  SiMobx,
 } from 'react-icons/si'
+
+import {AiFillFolderOpen,AiOutlineMobile,} from 'react-icons/ai'
 
 import { motion } from 'framer-motion'
 import { showHoverAnimation, removeHoverAnimation } from '../lib/windowAnimation'
@@ -25,12 +20,36 @@ import { FadeContainer, popUp } from '../lib/framerMotionVariants'
 
 const skills = [
   {
-    name: 'HTML',
-    logo: SiHtml5,
+    name: 'Flutter',
+    logo: SiFlutter,
   },
   {
-    name: 'CSS',
-    logo: SiCss3,
+    name: 'React',
+    logo: SiReact,
+  },
+  {
+    name: 'Firebase',
+    logo: SiFirebase,
+  },
+  
+  {
+    name: 'Gerenciamento de Estado',
+    logo: SiMobx,
+  },
+  {
+    name: 'Nextjs',
+    logo: SiNextdotjs,
+  },
+  
+  {
+    name: 'MySql',
+    logo: SiMysql,
+  },
+
+  
+  {
+    name: 'Dart',
+    logo: SiDart,
   },
   {
     name: 'JavaScript',
@@ -42,51 +61,18 @@ const skills = [
   },
 
   {
-    name: 'React',
-    logo: SiReact,
-  },
-  {
-    name: 'Nextjs',
-    logo: SiNextdotjs,
-  },
-
-  {
-    name: 'Tailwind CSS',
-    logo: SiTailwindcss,
-  },
-  {
-    name: 'Styled Components',
-    logo: SiStyledcomponents,
-  },
-  {
-    name: 'Sass',
-    logo: SiSass,
+    name: 'Arquitetura',
+    logo: AiFillFolderOpen,
   },
   {
     name: 'Git',
     logo: SiGit,
   },
+  {
+    name: 'Desenvolvimento Mobile',
+    logo: AiOutlineMobile,
+  },
 
-  {
-    name: 'MySql',
-    logo: SiMysql,
-  },
-  {
-    name: 'Desenvolvimento Android',
-    logo: SiAndroid,
-  },
-  {
-    name: 'Dart',
-    logo: SiDart,
-  },
-  {
-    name: 'Flutter',
-    logo: SiFlutter,
-  },
-  {
-    name: 'Firebase',
-    logo: SiFirebase,
-  },
 ]
 
 const Skills = () => {
@@ -110,7 +96,7 @@ const Skills = () => {
               key={skill.name}
               onMouseMove={(e) => showHoverAnimation(e)}
               onMouseLeave={(e) => removeHoverAnimation(e)}
-              className="dark:bg-darkPrimary hover:dark:bg-darkSecondary group flex origin-center transform items-center justify-center gap-4 rounded-sm border border-gray-300 p-4 dark:border-neutral-700 sm:justify-start md:origin-top"
+              className="dark:bg-darkPrimary hover:dark:bg-darkSecondary group flex origin-center transform items-center justify-center gap-4 rounded-sm border border-gray-300 hover:border-gray-400 p-4 dark:border-neutral-700 sm:justify-start md:origin-top"
             >
               <div className="pointer-events-none relative select-none transition group-hover:scale-110 sm:group-hover:scale-100">
                 <skill.logo className="h-8 w-8" />
