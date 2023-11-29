1802ee3b75f5d947893c3bf406a0ee0e91ac3097
Anderson-Andre-P <eleu.ander@gmail.com>
2023-11-29 19:06:40 -0300

    fix: adjust image size

--- components/Card.js
+++ components/Card.js
@@ -15,8 +15,8 @@ const Card = ({ title, description, imgSrc, href }) => (
               alt={title}
               src={imgSrc}
               className="object-cover object-center md:h-36 lg:h-48"
-              width={544}
-              height={306}
+              width={640}
+              height={320}
             />
           </Link>
         ) : (
@@ -24,8 +24,8 @@ const Card = ({ title, description, imgSrc, href }) => (
             alt={title}
             src={imgSrc}
             className="object-cover object-center md:h-36 lg:h-48"
-            width={544}
-            height={306}
+            width={640}
+            height={320}
           />
         ))}
       <div className="p-6">
